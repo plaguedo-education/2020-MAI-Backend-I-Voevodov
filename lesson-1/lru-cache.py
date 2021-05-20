85ce6c6ac875b17219a25f286332285209353db4
Igor Voevodov <voevodov97@mail.ru>
2021-05-20 21:40:11 +0300

    NEW: Init LRUCache class

---
+++ lesson-1/lru-cache.py
@@ -0,0 +1,14 @@
+class LRUCache():
+
+    def __init__(self, capacity: int=10) -> None:
+        self.capacity = capacity
+
+    def get(self, key: str) -> str:        
+        pass
+
+    def set(self, key: str, value: str) -> None:
+        pass
+
+    def rem(self, key: str) -> None:
+        pass
+        
