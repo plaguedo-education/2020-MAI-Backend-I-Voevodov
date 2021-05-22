a8262577179cf2c6f769ef0e516778fc643525cb
Igor Voevodov <voevodov97@mail.ru>
2021-05-22 19:21:02 +0300

    NEW: Update admin.py for companies

--- course project/human_hiring/companies/admin.py
+++ course project/human_hiring/companies/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import Companie, Vacancy, Responses
 
 # Register your models here.
+admin.site.register(Companie)
+admin.site.register(Vacancy)
+admin.site.register(Responses)
